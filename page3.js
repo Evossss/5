e97340f163067edc05733fd64e5431a1d900c20b
Evossss <118131736+Evossss@users.noreply.github.com>
2022-11-13 20:39:32 +0100

    Update page3.js

--- page3.js
+++ page3.js
@@ -3,7 +3,7 @@ var boutonValider = document.getElementById("boutonValider");
 
 boutonValider.onclick= function() {
      if (reponse.value === "Permanence"){
-        reponse.value=""; //lien de la page suivante 
+        reponse.value="https://evossss.github.io/6/"; 
     }
     else{reponse.value="Falsche Antwort";
 }
